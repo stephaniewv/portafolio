dfb3a74172382a6ef395d322a3b4256aa7853911
stephaniewv <stephaniewv@gmail.com>
2018-02-12 12:03:45 -0500

    upload changes

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#typed").typed({
+    strings: ["STEPHANIE WONG"],
+    typeSpeed: 50,
+    backDelay: 500,
+    loop: false,
+    loopCount: false,
+    callback: function () { foo(); }
+  });
+  function foo() { console.log("Callback"); }
+
+  $("#typedsecond").typed({
+    strings: ["UX Designer"],
+    typeSpeed: 50,
+    backDelay: 500,
+    loop: false,
+    loopCount: false,
+    callback: function () { foo(); }
+  });
+  function foo() { console.log("Callback");}
+
+});
